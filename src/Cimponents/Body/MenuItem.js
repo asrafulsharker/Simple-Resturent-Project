471d45221d761803d9bb1afe6b3e213438093a95
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-10-19 00:26:17 +0600

    designing menu item

--- src/Cimponents/Body/MenuItem.js
+++ src/Cimponents/Body/MenuItem.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardBody, CardTitle } from 'reactstrap';
 
-const MenuItem = props=>{
-    console.log(props);
+const MenuItem = props => {
+   
     return(
         <div>
+            <Card style={{margin:"10px"}}>
+                <CardBody>
+                    <CardImg 
+                    width="100%" 
+                    alt={props.dish.name} 
+                    src={props.dish.image} 
+                    style={{opacity:"0.5"}} />
+                    <CardImgOverlay>
+                        <CardTitle>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
 
         </div>
     )
